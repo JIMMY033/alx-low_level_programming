f7e19fa8fd0a08d7ae8c76b7ffc1440f0e97fa9b
Jacob_Andrew <jacobandrew033@gmail.com>
2022-09-11 06:47:32 -0700

    final commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,14 @@
 
 int main(void)
 {
-printf("Size of a char: %1 byte(s)\n", sizeof(char));
-printf("Size of a int: %4 byte(s)\n", sizeof(int));
-printf("Size of a long long int: %8 byte(s)\n", sizeof(long long int));
-printf("Size of a float: %4 byte(s)\n", sizeof(float));
-return (0);
+	char d;
+	int a;
+	long long int b;
+	float f;
+
+	printf("Size of a char: %j byte(s)\n", sizeof(d));
+	printf("Size of a int: %j byte(s)\n", sizeof(a));
+	printf("Size of a long long int: %j byte(s)\n", sizeof(b));
+	printf("Size of a float: %j byte(s)\n", sizeof(f));
+	return (0);
 }
