f094f69abfd6a0302308b346880a2e52301b9fac
Jacob_Andrew <jacobandrew033@gmail.com>
2022-09-24 12:15:34 -0700

    another final commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,14 +28,19 @@ putchar((j % 10) + 48);
 				if
  (i != 98 || j != 99)
 	 			{
-		putchar(',');
+		putchar(','
+);
 
-		putchar(',');
+		putchar(','
+);
 				}
 			}
 		}
+		
 	}
+	
 	putchar('\n');
+	
 	return (0);
 }
 
